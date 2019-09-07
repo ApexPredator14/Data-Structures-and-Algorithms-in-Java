7fc8d10a413c51cb7936df2145474b25417a051c
ApexPredator14 <36509857+ApexPredator14@users.noreply.github.com>
2019-09-07 15:38:35 +0530

    Update OctalToDecimal.java

--- src/main/java/com/packt/datastructuresandalg/lesson1/activity/octaltodecimal/OctalToDecimal.java
+++ src/main/java/com/packt/datastructuresandalg/lesson1/activity/octaltodecimal/OctalToDecimal.java
@@ -1,7 +1,25 @@
 package com.packt.datastructuresandalg.lesson1.activity.octaltodecimal;
 
 public class OctalToDecimal {
-    public int convertToDecimal(String octal) {
-        return 999;
+    public static int convertToDecimal(String s) 
+      {
+         int conver=1;
+         int result=0;
+        for(int i=1;i<=s.length();i++)
+        {
+        if(s.charAt(s.lenghth()-i)<8)
+      result+=s.charAt(s.length()-i*conver;
+                 conver*=8;
+                       }              
+     return result;
     }
+                  
+    public static void main(String args[])
+                       {   
+                           int res;
+                            Scanner sc= new Scanner(System.in);
+                            String oct= sc.nextLine();
+                           res = OctalToDecimal.convertToDecimal(oct);
+                        System.out.println(res);
+                       }
 }
